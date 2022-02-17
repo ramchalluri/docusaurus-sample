f4bcfcd89403b573f391e5ba12151f179699d18a
Ram Challuri <ramchalluri@gmail.com>
2022-02-17 12:17:39 +1100

    Adding last updated details

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
